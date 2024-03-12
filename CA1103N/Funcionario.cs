da60b42853d15f068d074cb05e2f3e3829b69369
Jeferson Ferreira <jeferson.oferreira@etec.sp.gov.br>
2024-03-11 22:03:17 -0300

    exemplos

---
+++ CA1103N/Funcionario.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CA1103N
+{
+    class Funcionario
+    {
+        public int matricula;
+        public string nome;
+        public string email;
+        public double salario;
+
+
+        public void cadastrar()
+        {
+
+            Console.WriteLine("Digite sua matricula:");
+            this.matricula = int.Parse(Console.ReadLine());
+            Console.WriteLine("Digite seu nome:");
+            this.nome = Console.ReadLine();
+            Console.WriteLine("Digite seu email");
+            this.email = Console.ReadLine();
+            Console.WriteLine("Digite o salario:");
+            this.salario = double.Parse(Console.ReadLine());
+        }
+
+        public void mostrarDados()
+        {
+            Console.WriteLine("Matricula:" + this.matricula);
+            Console.WriteLine("Nome:{0}", this.nome);
+            Console.WriteLine("E-mail:" + this.email);
+            Console.WriteLine("Salario: R${0}", this.salario);
+        }
+
+        public void descontaSalario()
+        {
+            Console.WriteLine("Digite o valor do desconto:");
+            double valorDesconto = double.Parse(Console.ReadLine());
+
+            while(valorDesconto <= 0)
+            {
+                Console.WriteLine("Valor incorreto!");
+                valorDesconto = this.setValorDesconto();
+            }
+            if(valorDesconto > 0)
+            {
+                this.salario = this.salario - valorDesconto;
+                Console.WriteLine("Valor descontado" + valorDesconto);
+            } else
+            {
+                Console.WriteLine("não houve desconto.");
+            }
+           
+        }
+
+        public void aumentaSalario(){
+            Console.WriteLine("Digite o valor do aumento:");
+            double valorAumento = double.Parse(Console.ReadLine());
+            this.salario = this.salario + valorAumento;
+            Console.WriteLine("Valor atualizado" + valorAumento);
+
+        }
+
+        public double setValorDesconto()
+        {
+            double valor;
+            Console.WriteLine("Digite um valor: ");
+            valor = double.Parse(Console.ReadLine());
+            return valor;
+        }
+    }
+}
---
+++ CA1103N/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CA1103N
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Funcionario funcionario1 = new Funcionario();
+            Funcionario funcionario2 = new Funcionario();
+            funcionario1.cadastrar();
+            funcionario1.mostrarDados();
+            funcionario1.descontaSalario();
+            funcionario1.mostrarDados();
+            Console.ReadKey();
+            funcionario2.cadastrar();
+            funcionario2.mostrarDados();
+            Console.ReadKey();
+        }
+    }
+}
